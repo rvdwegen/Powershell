ce4170bf535bbcfbe4574c109732d9cf0b29101d
Roel van der Wegen <rvdwegen@gmail.com>
2021-01-15 13:03:31 +0100

    Update DHCP-Search.ps1

--- DHCP-Search.ps1
+++ DHCP-Search.ps1
@@ -1,23 +1,43 @@
+<#
+.Synopsis
+  Search DHCP for a specified Hostname or MacAddress or export all leases
+.DESCRIPTION
+  This script searches the specified DHCP server for the requested Hostname or MacAddress
+.EXAMPLE 
+  .\DHCP-Search.ps1 -Type "Wildcard" -DHCPServer "DHCPhostname" -Export "DHCPLeases.csv" 
+.EXAMPLE
+  .\DHCP-Search.ps1 -Type "Hostname" -DHCPServer "DHCPhostname" -Hostname "FullOrPartialHostname" -Export "DHCPLeases.csv"
+.EXAMPLE
+  .\DHCP-Search.ps1 -Type "MacAddress" -DHCPServer "DHCPhostname" -MacAddress "FullOrPartialMacAddress" -Export "DHCPLeases.csv"
+#>
+
 [CmdletBinding()]
 param(
+    # Used to determine the log entry type
+    [Parameter(Mandatory=$true)]
+    [ValidateSet("Wildcard", "Hostname", "MacAddress")]
+    [String]$Type,
+
     # Set the DHCP server to query
     [Parameter(Mandatory=$true)]
     [String]$DHCPServer,
 
-    # Hostname to search for (can be partial match)
+    # Hostname to search for
     [parameter(Mandatory=$false)]
     [String]$Hostname="",
 
-    # CSV file location
+    # Hostname to search for
     [parameter(Mandatory=$false)]
-    [String]$Export="C:\Temp\DHCPLeases.csv"
+    [String]$MacAddress="",
+
+    # CSV file location
+    [parameter(Mandatory=$true)]
+    [String]$Export
 )
 
-# Main
 Function Start-Script {
     Try {
-        Get-DHCPLeases -DHCPServer $DHCPServer -Hostname $Hostname -Export $Export -ErrorAction Stop
-        Write-Log -Type "Info" -LogMsg "Script has been completed" -Console
+        Get-DHCPLeases -Type $Type -DHCPServer $DHCPServer -Hostname $Hostname -MacAddress $MacAddress -Export $Export -ErrorAction Stop
         Write-Log -Type "Info" -LogMsg "Output can be found at $Export" -Console
     }
     Catch {
@@ -25,34 +45,60 @@ Function Start-Script {
         Write-Log -Type "Error" -LogMsg $_.Exception.GetType().FullName -Console
         Write-Log -Type "Error" -LogMsg $_.Exception.Message -Console
     }
+    Finally {
+        Write-Log -Type "Info" -LogMsg "Script has been completed" -Console
+    }
 }
 
-# Gets the DHCP leases, searches them for matching hostnames and exports to $export
 Function Get-DHCPLeases {
     [CmdletBinding()]
     param(
+        # Used to determine the log entry type
+        [Parameter(Mandatory=$true)]
+        [ValidateSet("Wildcard", "Hostname", "MacAddress")]
+        [String]$Type,
+
         # Set the DHCP server to query
         [Parameter(Mandatory=$true)]
         [String]$DHCPServer,
 
         # Hostname to search for
         [parameter(Mandatory=$false)]
-        [String]$Hostname="",
+        [String]$Hostname,
+
+        # MacAddress to search for
+        [parameter(Mandatory=$false)]
+        [String]$MacAddress,
 
         # CSV file location
-        [parameter(Mandatory=$false)]
+        [parameter(Mandatory=$true)]
         [String]$Export
     )
+
+    # Determine the request type
+    switch ($Type) {
+        "Wildcard" { [int]$Type = 1 }
+        "Hostname" { [int]$Type = 2 }
+        "MacAddress" { [int]$Type = 3 }
+    }
 
     $Scopes = Get-DhcpServerv4Scope -ComputerName $DHCPServer -ErrorAction Stop
 
     $DHCPLeases = @()
 
     foreach ($ScopeID in $Scopes) {
-        $DHCPLeases += Get-DhcpServerv4Lease -ComputerName $DHCPServer -ScopeId $ScopeID.scopeid -ErrorAction Continue | Select-Object IPAddress, ScopeId, AddressState, HostName | Where-Object {$_.hostname -match $Hostname}
+        $DHCPLeases += Get-DhcpServerv4Lease -ComputerName $DHCPServer -ScopeId $ScopeID.scopeid -ErrorAction Continue
     }
 
-    $DHCPLeases | Export-Csv -Path $Export -delimiter ';' -NoTypeInformation -ErrorAction Stop
+    switch ($Type) {
+        # Wildcard, exports EVERYTHING
+        "1" { $DHCPLeases | Select-Object HostName, IPAddress, ClientId, ScopeId, AddressState | Export-Csv -Path $Export -delimiter ';' -NoTypeInformation -ErrorAction Stop }
+        # Hostname, exports everything that matches the hostname. Partial matches are possible
+        "2" { $DHCPLeases | Select-Object HostName, IPAddress, ClientId, ScopeId, AddressState | Where-Object {$_.Hostname -Match "$Hostname"} | Export-Csv -Path $Export -delimiter ';' -NoTypeInformation -ErrorAction Stop }
+        # MacAddress, exports everything that matches the MacAddress. Partial matches are possible
+        "3" { $DHCPLeases | Select-Object HostName, IPAddress, ClientId, ScopeId, AddressState | Where-Object {$_.ClientId -Match "$MacAddress"} | Export-Csv -Path $Export -delimiter ';' -NoTypeInformation -ErrorAction Stop }
+    }
+
 }
 
 # Log function
@@ -135,5 +181,4 @@ Function Write-Log{
     # ($LogDate + " " + $LogTime + " | Function: " + $GetFunctionName + " | Message: " + $LogMsg) # Example formatting if you need a full log entry in the Powershell Console
 } # End of logging function
 
-# Starts the script
 Start-Script
