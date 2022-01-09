c4e731c7c18153d954a7353f411920c66b56643b
Roel van der Wegen <rvdwegen@gmail.com>
2022-01-09 23:22:46 +0100

    Update Get-SourceForgeFile.ps1

--- Get-SourceForgeFile.ps1
+++ Get-SourceForgeFile.ps1
@@ -1,3 +1,3 @@
 # Example using MPV Player
-$filename = ((Invoke-RestMethod -Uri https://sourceforge.net/projects/mpv-player-windows/rss?path=/64bit | Select-Object -property link -Last 1).link.split("/")[-2])
+$filename = ((Invoke-RestMethod -Uri 'https://sourceforge.net/projects/mpv-player-windows/rss?path=/64bit').link[0].split("/")[-2])
 $fullurl = "https://download.sourceforge.net/mpv-player-windows/" + $filename
