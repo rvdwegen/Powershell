af05789cfa5fbad021cf8f114f53f2cf0c144fa0
Roel van der Wegen <rvdwegen@gmail.com>
2022-04-24 19:39:32 +0200

    Update Set-RebootAndRestartScriptTask.ps1

--- Set-RebootAndRestartScriptTask.ps1
+++ Set-RebootAndRestartScriptTask.ps1
@@ -1,43 +1,45 @@
-[CmdletBinding()]
-param(
-[Boolean]$Settask
-)
+Function Set-RebootAndRestartScriptTask {
+    [CmdletBinding()]
+    param(
+    [Boolean]$settask
+    )
 
-switch ($settask) {
-    $true {
-        # Define task action
-        $ActionParam = @{
-            Execute = 'Powershell.exe'
-            Argument = ("-ExecutionPolicy Bypass -File " + ($MyInvocation.ScriptName))
+    switch ($settask) {
+        $true {
+            # Define task action
+            $ActionParam = @{
+                Execute = 'Powershell.exe'
+                Argument = ("-ExecutionPolicy Bypass -File " + ($MyInvocation.ScriptName))
+            }
+
+            # Define task trigger
+            $TriggerParam = @{
+                AtLogOn = $True
+                User = (([System.Security.Principal.WindowsIdentity]::GetCurrent().Name).Split('\')[-1])
+            }
+
+            # Define task settings
+            $SettingsParam = @{
+                AllowStartIfOnBatteries = $true
+                DontStopIfGoingOnBatteries = $true
+            }
+
+            # Construct task
+            $Register = @{
+                Action = (New-ScheduledTaskAction @ActionParam)
+                Trigger = (New-ScheduledTaskTrigger @TriggerParam)
+                Settings = (New-ScheduledTaskSettingsSet @SettingsParam)
+                User = (([System.Security.Principal.WindowsIdentity]::GetCurrent().Name).Split('\')[-1])
+                TaskName = "PrepareScriptRebootTask"
+                Description = "PrepareScriptRebootTask"
+            }
+
+            # Register task
+            Register-ScheduledTask @Register
         }
-
-        # Define task trigger
-        $TriggerParam = @{
-            AtLogOn = $True
-            User = (([System.Security.Principal.WindowsIdentity]::GetCurrent().Name).Split('\')[-1])
+        $false {
+            Unregister-ScheduledTask -TaskName "PrepareScriptRebootTask" -Confirm:$false -ErrorAction SilentlyContinue
         }
-
-        # Define task settings
-        $SettingsParam = @{
-            AllowStartIfOnBatteries = $true
-            DontStopIfGoingOnBatteries = $true
-        }
-
-        # Construct task
-        $Register = @{
-            Action = (New-ScheduledTaskAction @ActionParam)
-            Trigger = (New-ScheduledTaskTrigger @TriggerParam)
-            Settings = (New-ScheduledTaskSettingsSet @SettingsParam)
-            User = (([System.Security.Principal.WindowsIdentity]::GetCurrent().Name).Split('\')[-1])
-            TaskName = "PrepareScriptRebootTask"
-            Description = "PrepareScriptRebootTask"
-        }
-
-        # Register task
-        Register-ScheduledTask @Register
+        Default {Throw "Something went wrong, this isn't supposed to happen"}
     }
-    $false {
-        Unregister-ScheduledTask -TaskName "PrepareScriptRebootTask" -Confirm:$false -ErrorAction SilentlyContinue
-    }
-    Default {Throw "Something went wrong, this isn't supposed to happen"}
 }
