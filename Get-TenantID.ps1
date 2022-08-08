6b7d69b12a8f450430ca26a35305dbd037a7ea47
Roel van der Wegen <github@aeternus.tech>
2022-08-08 12:07:01 +0200

    Added function example

--- Get-TenantID.ps1
+++ Get-TenantID.ps1
@@ -1,2 +1,27 @@
+# Oneliner
 $UserPrincipalName = "Bill@Contoso.com" # For example: Bill@Contoso.com
 (Invoke-RestMethod "https://login.windows.net/$($UserPrincipalName.Split("@")[1])/.well-known/openid-configuration" -Method GET).userinfo_endpoint.Split("/")[3]
+
+# Or use as a function,
+
+Function Get-TenantID {
+    param (
+        [Parameter(Mandatory)]
+        [String]$UserPrincipalName
+    )
+
+    $Tenant = $($UserPrincipalName.Split("@")[1])
+
+    $Params = @{
+        Method = 'GET'
+        Uri = "https://login.windows.net/$Tenant/.well-known/openid-configuration"
+    }
+
+    $Payload = Invoke-RestMethod @Params
+
+    $TenantID = $Payload.userinfo_endpoint.Split("/")[3]
+
+    Return $TenantID
+}
+
+Get-TenantID -UserPrincipalName "Bill@Contoso.com"
