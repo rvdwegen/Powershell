3dacc3e8f1e8446d607602bc9bfeff4edbc4879f
Roel van der Wegen <github@aeternus.tech>
2022-08-29 10:32:53 +0200

    Update Test-InternetConnection.ps1

--- Test-InternetConnection.ps1
+++ Test-InternetConnection.ps1
@@ -1,5 +1,5 @@
 Function Test-InternetConnection {
-    # Lists outside connections that are not the VPN
+    # Lists outside connections
     $Status = Get-NetRoute | Where-Object DestinationPrefix -eq '0.0.0.0/0' | Get-NetIPInterface | Where-Object {($_.ConnectionState -eq 'Connected')}
 
     # If someone finds a list of possible default InterfaceAlias results please share it
