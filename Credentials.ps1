39578bdc9668dcd8c5ad5c5574678aab8e753804
Roel van der Wegen <github@aeternus.tech>
2022-08-29 10:27:25 +0200

    Delete Credentials.ps1

--- Credentials.ps1
+++
@@ -1,4 +0,0 @@
-# Do not hardcode credentials in production, obviously.
-$username = 'Account'
-$password = ConvertTo-SecureString 'Password' -asplaintext -force;
-$cred = New-Object -TypeName PSCredential -argumentlist $username, $password
