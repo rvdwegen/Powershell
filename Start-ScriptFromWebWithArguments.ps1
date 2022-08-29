ae524ffb72e363bdc4bf3bab4cf6d7e957190e03
Roel van der Wegen <github@aeternus.tech>
2022-08-29 10:33:44 +0200

    Delete Start-ScriptFromWebWithArguments.ps1

--- Start-ScriptFromWebWithArguments.ps1
+++
@@ -1,1 +0,0 @@
-$url = & $([scriptblock]::Create((New-Object Net.WebClient).DownloadString("https://raw.githubusercontent.com/pbatard/Fido/master/Fido.ps1"))) -Win "Windows 10" -Ed "Pro" -Arch "x64" -GetUrl
