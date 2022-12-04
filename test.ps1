2a2ea75831fed8b39f5ab705f493a3c314acf965
Roel van der Wegen <github@aeternus.tech>
2022-12-04 16:06:50 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,2 @@
-Write-Host "test host"
-Write-Output "test-output"
-"test-direct"
-New-Item -Path "C:\ProgramData\Scoop\test.txt" -ItemType "file" -Value "This is a text string."
+$name = split-path (whoami) -leaf
+"Hello, $name!"
