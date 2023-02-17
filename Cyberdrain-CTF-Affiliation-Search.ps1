32d59f4c352a53a052efa8c722a9e3cc39f1ed26
Roel van der Wegen <github@aeternus.tech>
2023-02-17 10:20:59 +0100

    Update Cyberdrain-CTF-Affiliation-Search.ps1

--- Cyberdrain-CTF-Affiliation-Search.ps1
+++ Cyberdrain-CTF-Affiliation-Search.ps1
@@ -1,1 +1,5 @@
+# Single affiliation
 $affiliation = "officegrip"; (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users?affiliation=$($affiliation)").data | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users/$($_.id)").data } | Sort-Object -Property score -Descending | ForEach-Object { Write-Host "$($_.name): $($_.place) place with a score of $($_.score)" }
+
+# Multiple affiliations
+@("officegrip", "Prime Networks") | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users?affiliation=$($_)").data } | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users/$($_.id)").data } | Sort-Object -Property score -Descending | ForEach-Object { Write-Host "$($_.name) from $($_.affiliation): $($_.place) place with a score of $($_.score)" }
