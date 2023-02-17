44cd8c6c79df45c87fe2198ca9b5f8595ab6d543
Roel van der Wegen <github@aeternus.tech>
2023-02-17 10:29:23 +0100

    Update Cyberdrain-CTF-Affiliation-Search.ps1

--- Cyberdrain-CTF-Affiliation-Search.ps1
+++ Cyberdrain-CTF-Affiliation-Search.ps1
@@ -3,3 +3,6 @@ $affiliation = "officegrip"; (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.
 
 # Multiple affiliations
 @("officegrip", "Prime Networks") | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users?affiliation=$($_)").data } | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users/$($_.id)").data } | Sort-Object -Property score -Descending | ForEach-Object { Write-Host "$($_.name) from $($_.affiliation): $($_.place) place with a score of $($_.score)" }
+
+# Multiple affiliations with Out-GridView
+@("officegrip", "Prime Networks") | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users?affiliation=$($_)").data } | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users/$($_.id)").data } | Select-Object -Property id, name, affiliation, country, place, score | Sort-Object -Property score -Descending | Out-GridView -Title "Scoreboard"
