239a986fac50ca5b182ec99c1cb934e17ef7352a
Roel van der Wegen <github@aeternus.tech>
2023-02-18 13:15:15 +0100

    Update and rename Cyberdrain-CTF-Affiliation-Search.ps1 to Cyberdrain-CTF-Search.ps1

--- Cyberdrain-CTF-Search.ps1
+++ Cyberdrain-CTF-Search.ps1
@@ -6,3 +6,8 @@ $affiliation = "officegrip"; (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.
 
 # Multiple affiliations with Out-GridView
 @("officegrip", "Prime Networks") | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users?affiliation=$($_)").data } | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "https://ctf.cyberdrain.com/api/v1/users/$($_.id)").data } | Select-Object -Property id, name, affiliation, country, place, score | Sort-Object -Property score -Descending | Out-GridView -Title "Scoreboard"
+
+# Get all affiliated users
+$baseUri = "https://ctf.cyberdrain.com/api/v1/users"
+$pages = (Invoke-RestMethod -Method 'GET' -Uri $baseUri).meta.pagination.pages
+1..$pages | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri ($baseUri + "?page=" + $_)).data } | Where-Object { $_.affiliation } | Select-Object affiliation -Unique | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "$($baseUri)?affiliation=$($_.affiliation)").data } | ForEach-Object { (Invoke-RestMethod -Method 'GET' -Uri "$($baseUri)/$($_.id)").data } | Sort-Object -Property score -Descending | ForEach-Object { Write-Host "$($_.name) from $($_.affiliation): $($_.place) place with a score of $($_.score)" }
