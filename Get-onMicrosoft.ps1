c82675569705743ca003254b0949434e294b190b
Roel van der Wegen <github@aeternus.tech>
2023-03-26 22:59:03 +0200

    Update Get-onMicrosoft.ps1

--- Get-onMicrosoft.ps1
+++ Get-onMicrosoft.ps1
@@ -1,2 +1,5 @@
 # Assumes you are already connected to Graph and have relevant permissions
+
+(Invoke-RestMethod -Method 'GET' -Headers $header -Uri "https://graph.microsoft.com/v1.0/domains?").value | Where-Object { $_.isInitial -eq $true }
+
 $onMicrosoft = ((Invoke-MgGraphRequest -Method 'GET' -Uri "https://graph.microsoft.com/v1.0/domains?`$select=id").value | Where-Object { $_.id -like('*.onmicrosoft.com') }).id
