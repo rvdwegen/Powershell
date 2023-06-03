1c379abc15ba61b5a90406001e6e319cee429789
Roel van der Wegen <github@aeternus.tech>
2023-06-03 23:25:10 +0200

    Update autopilot.ps1

--- autopilot.ps1
+++ autopilot.ps1
@@ -2,7 +2,7 @@
 if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
         $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
         Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
-        Exit
+        pause
 }
 
 function Save-File ([string]$filename) {
