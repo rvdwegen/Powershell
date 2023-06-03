954af38067c0cb0958fc15ee9eb74fee7410f61c
Roel van der Wegen <github@aeternus.tech>
2023-06-03 22:59:20 +0200

    Update scaffold4.ps1

--- scaffold4.ps1
+++ scaffold4.ps1
@@ -3,8 +3,8 @@ cls
 $script = @"
       ___        _              _ _       _     _____            __  __      _     _ 
      / _ \      | |            (_) |     | |   /  ___|          / _|/ _|    | |   | |
-    / /_\ \_   _| |_ ___  _ __  _| | ___ | |_  \ `--.  ___ __ _| |_| |_ ___ | | __| |
-    |  _  | | | | __/ _ \| '_ \| | |/ _ \| __|  `--. \/ __/ _` |  _|  _/ _ \| |/ _` |
+    / /_\ \_   _| |_ ___  _ __  _| | ___ | |_  \ `--.  ___ __ _| |_| |_ ___ | | __  | |
+    |  _  | | | | __/ _ \| '_ \| | |/ _ \| __|  `--. \/ __/ _` |  _|  _/ _ \| |/ _`   |
     | | | | |_| | || (_) | |_) | | | (_) | |_  /\__/ / (_| (_| | | | || (_) | | (_| |
     \_| |_/\__,_|\__\___/| .__/|_|_|\___/ \__| \____/ \___\__,_|_| |_| \___/|_|\__,_|
                         | |                                                         
@@ -12,17 +12,13 @@ $script = @"
 
 
               ============ OfficeGrip autopilot hash scaffold ============
-                                        v1.0
+                                        v1.1
 
 "@
 
 Write-Host $script
 
-$ScriptData = @'
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false
-iwr "https://raw.githubusercontent.com/rvdwegen/Powershell/main/autopilot.ps1" | iex 
-~
-'@
+$ScriptData = 'Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false; iwr "https://raw.githubusercontent.com/rvdwegen/Powershell/main/autopilot.ps1" | iex ~'
 
 $wshell = New-Object -ComObject wscript.shell;
 #$wshell.AppActivate('title of the application window')
