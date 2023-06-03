129aa29047f952fe6ee3618f2d615f03150b0eca
Roel van der Wegen <github@aeternus.tech>
2023-06-03 21:28:14 +0200

    Update scaffold.ps1

--- scaffold.ps1
+++ scaffold.ps1
@@ -1,4 +1,15 @@
 $script = @"
+  ___        _              _ _       _     _                 _                            
+ / _ \      | |            (_) |     | |   | |               | |                           
+/ /_\ \_   _| |_ ___  _ __  _| | ___ | |_  | |__   ___   ___ | |_ ___  ___ _ __ __ _ _ __  
+|  _  | | | | __/ _ \| '_ \| | |/ _ \| __| | '_ \ / _ \ / _ \| __/ __|/ __| '__/ _` | '_ \ 
+| | | | |_| | || (_) | |_) | | | (_) | |_  | |_) | (_) | (_) | |_\__ \ (__| | | (_| | |_) |
+\_| |_/\__,_|\__\___/| .__/|_|_|\___/ \__| |_.__/ \___/ \___/ \__|___/\___|_|  \__,_| .__/ 
+                     | |                                                            | |    
+                     |_|                                                            |_|    
+
+                ============ OfficeGrip autopilot hash bootstrap ============
+
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
 iwr "https://raw.githubusercontent.com/rvdwegen/Powershell/main/autopilot.ps1" | iex
 "@
