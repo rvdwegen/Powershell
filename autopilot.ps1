51e18ad93a3de9e00086367fa4367c8f26febbe1
Roel van der Wegen <github@aeternus.tech>
2023-06-03 21:25:09 +0200

    Update autopilot.ps1

--- autopilot.ps1
+++ autopilot.ps1
@@ -1,4 +1,15 @@
 #Requires -RunAsAdministrator
 
+function Save-File {
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+
+    $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+    $OpenFileDialog.initialDirectory = $env:USERPROFILE
+    $OpenFileDialog.filter = 'CSV (*.csv)|*.csv'
+    [void]$OpenFileDialog.ShowDialog()
+
+    return $OpenFileDialog.filename
+}
+
 Install-Script -name Get-WindowsAutopilotInfo -Force
-Get-WindowsAutopilotInfo
+Get-WindowsAutopilotInfo -OutputFile (Save-File)
