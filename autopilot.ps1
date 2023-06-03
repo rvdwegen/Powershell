c907d856591ba734db10a5d471d96ca063ef562c
Roel van der Wegen <github@aeternus.tech>
2023-06-03 23:13:05 +0200

    Update autopilot.ps1

--- autopilot.ps1
+++ autopilot.ps1
@@ -6,10 +6,13 @@ function Save-File {
     $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
     $OpenFileDialog.initialDirectory = $env:USERPROFILE
     $OpenFileDialog.filter = 'CSV (*.csv)|*.csv'
+    $OpenFileDialog.DefaultFileName = "test.csv"
     [void]$OpenFileDialog.ShowDialog()
 
     return $OpenFileDialog.filename
 }
+
+$SerialNumber = (Get-WmiObject win32_bios | select Serialnumber).SerialNumber
 
 Install-Script -name Get-WindowsAutopilotInfo -Force
 Get-WindowsAutopilotInfo -OutputFile (Save-File)
