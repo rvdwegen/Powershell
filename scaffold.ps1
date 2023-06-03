ba14d7a6e7437209bff5aa0c826bc987d432b927
Roel van der Wegen <github@aeternus.tech>
2023-06-03 22:56:10 +0200

    Update and rename scaffold2.ps1 to scaffold.ps1

---
+++ scaffold.ps1
@@ -0,0 +1,29 @@
+cls
+
+$script = @"
+      ___        _              _ _       _     _____            __  __      _     _ 
+     / _ \      | |            (_) |     | |   /  ___|          / _|/ _|    | |   | |
+    / /_\ \_   _| |_ ___  _ __  _| | ___ | |_  \ `--.  ___ __ _| |_| |_ ___ | | __| |
+    |  _  | | | | __/ _ \| '_ \| | |/ _ \| __|  `--. \/ __/ _` |  _|  _/ _ \| |/ _` |
+    | | | | |_| | || (_) | |_) | | | (_) | |_  /\__/ / (_| (_| | | | || (_) | | (_| |
+    \_| |_/\__,_|\__\___/| .__/|_|_|\___/ \__| \____/ \___\__,_|_| |_| \___/|_|\__,_|
+                        | |                                                         
+                        |_|                                                         
+
+
+              ============ OfficeGrip autopilot hash scaffold ============
+                                        v1.0
+
+"@
+
+Write-Host $script
+
+$ScriptData = @'
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false
+iwr "https://raw.githubusercontent.com/rvdwegen/Powershell/main/autopilot.ps1" | iex 
+~
+@'
+
+$wshell = New-Object -ComObject wscript.shell;
+#$wshell.AppActivate('title of the application window')
+$wshell.SendKeys($lol)
