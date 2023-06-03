1d6a6cebc284f55022264d9ea63875db8e661c5a
Roel van der Wegen <github@aeternus.tech>
2023-06-03 21:28:55 +0200

    Update scaffold.ps1

--- scaffold.ps1
+++ scaffold.ps1
@@ -10,8 +10,11 @@ $script = @"
 
                 ============ OfficeGrip autopilot hash bootstrap ============
 
+Copy paste the following commands:
+
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted
 iwr "https://raw.githubusercontent.com/rvdwegen/Powershell/main/autopilot.ps1" | iex
+
 "@
 
 Write-Host $script
