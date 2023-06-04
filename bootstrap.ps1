8ba9f5ffaa7d613b38e95ac30c77d933f22cfeeb
Roel van der Wegen <github@aeternus.tech>
2023-06-04 11:14:30 +0200

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -25,7 +25,24 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
         exit
 }
 
-$ScriptData = 'Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false; iwr "https://raw.githubusercontent.com/rvdwegen/Powershell/main/autopilot.ps1" | iex {ENTER}'
-
+#$ScriptData = 'Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false; iwr "https://raw.githubusercontent.com/rvdwegen/Powershell/main/autopilot.ps1" | iex {ENTER}'
+$ScriptData = 'Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false {ENTER}'
 $wshell = New-Object -ComObject wscript.shell
 $wshell.SendKeys($ScriptData)
+
+function Save-File ([string]$filename) {
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+
+    $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+    $OpenFileDialog.initialDirectory = "::{20D04FE0-3AEA-1069-A2D8-08002B30309D}"
+    $OpenFileDialog.filter = 'CSV (*.csv)|*.csv'
+    $OpenFileDialog.FileName = "$filename.csv"
+    [void]$OpenFileDialog.ShowDialog()
+
+    return $OpenFileDialog.filename
+}
+
+$SerialNumber = (Get-WmiObject win32_bios | select Serialnumber).SerialNumber
+
+Install-Script -name Get-WindowsAutopilotInfo -Force
+Get-WindowsAutopilotInfo -OutputFile (Save-File -filename $SerialNumber)
